df417ac138a9fabc2b47977ec2da41d8a0b3da9a
Gabriel Marinho <54584250@PMSBC.GOV.BR>
2026-06-18 14:12:11 -0300

    conectando banco de dados h2

--- src/main/java/com/educandoweb/couse/resources/UserResource.java
+++ src/main/java/com/educandoweb/couse/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.educandoweb.couse.resources;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import com.educandoweb.couse.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+		@GetMapping
 		public ResponseEntity<User> findAll(){
 			User u = new User(1L,"Maria", "maria@gmail.com","99999999","654321");
 			return ResponseEntity.ok().body(u);	}
